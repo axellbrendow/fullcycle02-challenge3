c4c80f7d0fe89b54aa71aa3b8262d87499a8f746
axell-brendow <breno.axel@gmail.com>
2020-05-09 23:43:16 -0300

    Remove service and add route /

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,22 @@
 import { Controller, Get, Render } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  @Get()
+  @Render('index')
+  index() {
+    //
+  }
 
   @Get('sender')
   @Render('index')
   sender() {
-    return { message: this.appService.getHello() };
+    //
   }
 
   @Get('receiver')
   @Render('index')
   receiver() {
-    return { message: this.appService.getHello() };
+    //
   }
 }
